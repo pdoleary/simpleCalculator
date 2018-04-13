319d2151e3906aac619d2cc12533ab6698d78767
otisliddy <otisliddy@gmail.com>
2018-04-13 12:26:50 +0100

    Added JS support for calculate() on add, multiply, divide, subtract

--- WebContent/calculator.js
+++ WebContent/calculator.js
@@ -1,13 +1,64 @@
 function calculate(expressionWithSpaces) {
-  expression = expressionWithSpaces.replace(/\s/g, '');
-  checkCorrectNumberOfBrackets(expression);
-
-  bracketsTerm = findBracketTerm(expression, startIndex)
-  return 0;
+  var expression = expressionWithSpaces.replace(/\s/g, '');
+  // checkCorrectNumberOfBrackets(expression);
+  // var bracketsTerm = findBracketTerm(expression, startIndex);
+  expression = resolveOperation(expression, '/');
+  expression = resolveOperation(expression, '*');
+  expression = resolveOperation(expression, '+');
+  expression = resolveOperation(expression, '-');
+  alert(expression);
+  return parseFloat(expression);
 }
 
+function resolveOperation(expression, operation) {
+  if (!expression.includes(operation)) { return expression; }
+  var divideSignIndex = expression.indexOf(operation);
+  var firstNum = getNumbersBeforeAndAfterIndex(expression, divideSignIndex).firstNum;
+  var secondNum = getNumbersBeforeAndAfterIndex(expression, divideSignIndex).secondNum;
+  var result = calculateSingleOperation(firstNum, secondNum, operation);
+
+  var expressionAfterCalculation = expression.replace(firstNum + operation
+          + secondNum, result);
+  return resolveOperation(expressionAfterCalculation, operation);
+}
+
+function getNumbersBeforeAndAfterIndex(expression, index) {
+  var partsBeforeDivideSign = expression.substring(0, index).split(
+          /[-\\+\\*\/]+/);
+  var firstNum = partsBeforeDivideSign[partsBeforeDivideSign.length - 1];
+  var partsAfterDivideSign = expression.substring(index + 1).split(
+          /[-\\+\\*\/]+/);
+  var secondNum = partsAfterDivideSign[0];
+  return {
+    firstNum: firstNum,
+    secondNum: secondNum
+  }
+}
+
+function calculateSingleOperation(firstNum, secondNum, operation) {
+  switch (operation) {
+  case '/':
+    return divide(firstNum, secondNum);
+    break;
+  case '*':
+    return multiply(firstNum, secondNum);
+    break;
+  case '+':
+    return add(firstNum, secondNum);
+    break;
+  case '-':
+    return subtract(firstNum, secondNum);
+    break;
+  default:
+    throw 'Undefined operation';
+  }
+}
+// TODO doesn't work with nested brackets
 function findBracketTerm(expression, startIndex) {
-  return '';
+  var firstLeftBracket = expression.indexOf('(');
+  var firstRightBracket = expression.indexOf(')');
+  var result = expression.substring(firstLeftBracket + 1, matchingRightBracket);
+  return result;
 }
 
 function checkCorrectNumberOfBrackets(expression) {
@@ -15,21 +66,21 @@ function checkCorrectNumberOfBrackets(expression) {
 }
 
 function add(num1, num2) {
-  var result = num1 + num2;
+  var result = parseFloat(num1) + parseFloat(num2);
   return result;
 }
 
 function subtract(num1, num2) {
-  var result = num1 - num2;
+  var result = parseFloat(num1) - parseFloat(num2);
   return result;
 }
 
 function multiply(num1, num2) {
-  var result = num1 * num2;
+  var result = parseFloat(num1) * parseFloat(num2);
   return result;
 }
 
 function divide(num1, num2) {
-  var result = num1 / num2;
+  var result = parseFloat(num1) / parseFloat(num2);
   return result;
 }
