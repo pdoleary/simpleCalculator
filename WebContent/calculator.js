60ae092c134cb7cf405849978d6f21159e161d4a
otisliddy <otisliddy@gmail.com>
2018-04-13 11:28:08 +0100

    JS support for add, subtract, multiply, divide

---
+++ WebContent/calculator.js
@@ -0,0 +1,19 @@
+function add(num1, num2){
+  var result = num1 + num2;
+  return return result;
+}
+
+function subtract(num1, num2){
+  var result = num1 - num2;
+  return return result;
+}
+
+function multiply(num1, num2){
+  var result = num1 * num2;
+  return return result;
+}
+
+function divide(num1, num2){
+  var result = num1 / num2;
+  return return result;
+}
